790640579ecc372d45104f6ab3a487759b28e91b
kirillv8 <urmancheev.it@gmail.com>
2025-02-10 02:39:25 +0300

    feature/ aded function getGenres

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
-import './App.scss';
-import RegistrationModal from './components/Registration/RegistrationModal';
-import AuthorizationModal from './components/Authorization/Authorization';
-import Header from './components/Header/Header';
-import Filters from './components/Filters/Filters';
-
+import { useState } from "react";
+import "./App.scss";
+import RegistrationModal from "./components/Registration/RegistrationModal";
+import AuthorizationModal from "./components/Authorization/Authorization";
+import Header from "./components/Header/Header";
+import Filters from "./components/Filters/Filters";
+import { getGenres } from "./api/moviesApi";
 
 const App: React.FC = () => {
   const [isVisibleReg, setIsVisibleReg] = useState<boolean>(false);
   const [isVisibleAuth, setIsVisibleAuth] = useState<boolean>(false);
+  const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+  const token =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTIwMDQ1ZmZjNDU2Y2ZlNTdmYWFhOTBiNTFjMTk2YSIsIm5iZiI6MTcyMjUyNzM0OS4wODgsInN1YiI6IjY2YWJhZTc1NGZlNDIxMzEwY2QyY2M4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tiiJIjyQwzyPjCE1wkYwAyuFwBAH48g_V2plF_4kwow";
+  const result = getGenres(url, token);
+  console.log(result);
   return (
     <>
       {isVisibleReg && <RegistrationModal />}
--- src/api/moviesApi.ts
+++ src/api/moviesApi.ts
@@ -1,1 +1,19 @@
-
+export const getGenres = async (url: string, token: string) => {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error(`Ошибка: ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
