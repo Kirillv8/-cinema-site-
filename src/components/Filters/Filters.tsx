e71d79d77f522c8cf31348a484d6578297954d45
kirillv8 <urmancheev.it@gmail.com>
2025-04-17 20:31:26 +0300

    fix/ Changed the location of the filter components

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,16 +1,22 @@
 import { useState, useContext } from "react";
+import { ChangeEvent } from "react";
+import { Box, Typography, Divider } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+
 import Pagination from "../Pagination/Pagination";
 import MovieList from "../MovieList/MovieList";
-import { ChangeEvent } from "react";
-import { ContextOptionGenres } from "../../context/ContextForFlters/ContextForFilters";
-import { ContextOptionsYears } from "../../context/ContextForFlters/ContextForFilters";
-import { useChangeDispatch } from "../../context/ContextForFlters/ContextForFilters";
-import { IconButton, Box, Typography } from "@mui/material";
-import { Delete } from "@mui/icons-material";
 import SliderComponent from "../Slider/Slider";
+import SortMovies from "./SortMovies";
+
+import {
+  ContextOptionGenres,
+  ContextOptionsYears,
+  useChangeDispatch,
+} from "../../context/ContextForFlters/ContextForFilters";
 
 const Filters = () => {
   const [selectedGenres, setSelectedGenres] = useState<Set<string>>(new Set());
+
   const optionsGenres = useContext(ContextOptionGenres);
   const optionsYears = useContext(ContextOptionsYears);
   const dispatch = useChangeDispatch();
@@ -23,32 +29,56 @@ const Filters = () => {
   };
 
   return (
-    <>
-      <Box component="section">
-        <Box component="div">
-          <Typography variant="h5">Фильтры</Typography>
-        </Box>
-      </Box>
-      <Box>
-        <IconButton aria-label="fingerprint" color="success">
-          {" "}
-          <Delete />
-        </IconButton>
-      </Box>
-      <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: 4,
+        padding: 4,
+        flexWrap: "wrap",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          minWidth: 250,
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Фильтры
+        </Typography>
+
         <SliderComponent />
-      </Box>
-      <Box>
+
         <MovieList
           selectedGenres={selectedGenres}
           setSelectedGenres={setSelectedGenres}
         />
+        <Box>
+          <Pagination />
+        </Box>
+        {/* <SortMovies /> */}
       </Box>
 
-      <Box>
-        <Pagination />
+      <Divider orientation="vertical" flexItem />
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 4,
+          flex: 1,
+          maxWidth: "1000px",
+        }}
+      >
+        {/* Карточки фильмов от SortMovies */}
+        {/* <SortMovies /> тут карточки фильмов добавить */}
       </Box>
-    </>
+    </Box>
   );
 };
 
