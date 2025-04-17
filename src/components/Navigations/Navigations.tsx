f385bc52c0c91e2308e31b292ec92f21650b48af
kirillv8 <urmancheev.it@gmail.com>
2025-04-17 16:29:10 +0300

    fix/reduced the size of pagination buttons and removed unnecessary imports

--- src/components/Navigations/Navigations.tsx
+++ src/components/Navigations/Navigations.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
 
 interface PropId {
   filmId: number;
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,37 +1,34 @@
-import imageArrowLeft from "../../assets/free-icon-left.png";
-import imageArrowRight from "../../assets/free-icon-right.png";
 import { Box, Button, ListItem } from "@mui/material";
 import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const Pagination = () => {
   const numbersButton = [1, 2, 3, 4, 5];
+
   const res = numbersButton.map((number) => {
     return (
-      <ListItem key={`number: ${number}`}>
-        <Button variant="outlined">{number}</Button>
+      <ListItem
+        key={`number: ${number}`}
+        sx={{ padding: "2px", width: "auto" }}
+      >
+        <Button variant="outlined" size="small">
+          {number}
+        </Button>
       </ListItem>
     );
   });
 
   return (
-    <>
-      <Box sx={{ display: "flex" }}>
-        <Box sx={{ display: "flex", justifyContent: "start" }}>
-          <Button>
-            <ArrowBackIos />
-          </Button>
-        </Box>
+    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+      <Button size="small">
+        <ArrowBackIos fontSize="small" />
+      </Button>
 
-        <Box sx={{ display: "flex", justifyContent: "center" }}>{res}</Box>
+      <Box sx={{ display: "flex", gap: 1 }}>{res}</Box>
 
-        <Box sx={{ display: "flex", justifyContent: "end" }}>
-          <Button>
-            {" "}
-            <ArrowForwardIos />
-          </Button>
-        </Box>
-      </Box>
-    </>
+      <Button size="small">
+        <ArrowForwardIos fontSize="small" />
+      </Button>
+    </Box>
   );
 };
 
